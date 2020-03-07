a659e41cf6a7a4b782fd1cc60c291c773fdc9052
2521588650 <2521588650@qq.com>
2020-03-07 14:41:47 +0800

    茶世河提交

--- egg/app/router.js
+++ egg/app/router.js
@@ -5,14 +5,31 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.post('/login', controller.login.index);
-  router.get('/getData', controller.getData.index);
-  router.get('/getMovie', controller.getMovie.index);
-  router.get('/getWantMovie', controller.getWantMovie.index);
-  router.get('/getStars', controller.getStars.index);
-  router.get('/getComment', controller.getComment.index);
-  router.post('/addWant', controller.addWant.index);
-  router.post('/addComment', controller.addComment.index);
-  router.post('/addNum', controller.addNum.index);
-  router.post('/delWant', controller.delWant.index);
+  router.get('/', controller.home.index);
+  router.post('/post',controller.post.index);
+  router.get('/get', controller.get.index);
+  //实时查询数据
+  router.get('/searchtime', controller.get.searchtime);
+  //首页查询所有店铺信息
+  router.post('/index', controller.getinfo.index);
+  //根据类型id查询店铺信息
+  router.post('/Tshop', controller.getshop.byshtid);
+  //根据id查询店铺信息
+  router.post('/typeshop', controller.getshop.index);
+  //根据店铺id查询团购信息
+  router.post('/getgroup', controller.getgroup.index);
+  //根据团购id查询团购信息
+  router.post('/getgroupbyid', controller.getgroup.groupid);
+  //获取店铺种类信息
+  router.post('/gettypeimg', controller.gettype.index);
+
+
+  //加入购物车信息
+  router.post('/addcar', controller.addbuy.index);
+
+  
+
+  
+
+
 };
--- vuecli-front/src/router/index.js
+++ vuecli-front/src/router/index.js
@@ -1,60 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const VueRouterPush = VueRouter.prototype.push
-VueRouter.prototype.push = function push(to) {
-  return VueRouterPush.call(this, to).catch(err => err)
-}
-
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/dlc',
+    name: 'Delicious',
+    component: () => import('../views/Delicious.vue')
   },
   {
-    path: '/movie',
-    name: 'Movie',
-    component: () => import('../views/Movie.vue'),
+    path: '/sortv',
+    name: 'Sortv',
+    component: () => import('../components/sortview.vue')
   },
   {
-    path: "/cinema",
-    name: 'Cinema',
-    component: () => import('../components/movie/Cinema.vue')
+    path: '/shopv',
+    name: 'Shopv',
+    component: () => import('../components/shopview.vue')
   },
   {
-    path: "/my",
-    name: 'My',
-    component: () => import('../components/movie/My.vue')
+    path: '/groupinfo',
+    name: 'Groupinfo',
+    component: () => import('../components/groupinfo.vue')
   },
   {
-    path: "/detail",
-    name: 'Detail',
-    component: () => import('../components/movie/Detail.vue')
+    path: '/buying',
+    name: 'Buying',
+    component: () => import('../components/buying.vue')
   },
   {
-    path: '/ktv',
-    name: 'Ktv',
-    component: () => import('../views/Ktv.vue')
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
-  },
-  {
-    path: '/food',
-    name: 'Food',
-    component: () => import('../views/Food.vue')
-  },
-  {
-    path: '/beauty',
-    name: 'Beauty',
-    component: () => import('../views/Beauty.vue')
-  },
+    path: '/search',
+    name: 'Search',
+    component: () => import('../components/search.vue')
+  }
+
 ]
 
 const router = new VueRouter({
@@ -62,5 +42,7 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+
 
 export default router
